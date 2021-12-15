4927c375f3c1bddec330131cb8f262da3a29c0c5
Omar <Omar EZKER>
2021-12-15 22:27:17 +0100

    start new order

--- src/App.js
+++ src/App.js
@@ -11,21 +11,36 @@ const truncate = (input, len) =>
 
 export const StyledButton = styled.button`
   padding: 10px;
-  border-radius: 50px;
   border: none;
   background-color: var(--secondary);
   padding: 10px;
   font-weight: bold;
-  color: var(--secondary-text);
+  color: var(--primary);
   width: 100px;
   cursor: pointer;
-  box-shadow: 0px 6px 0px -2px rgba(250, 250, 250, 0.3);
-  -webkit-box-shadow: 0px 6px 0px -2px rgba(250, 250, 250, 0.3);
-  -moz-box-shadow: 0px 6px 0px -2px rgba(250, 250, 250, 0.3);
-  :active {
-    box-shadow: none;
-    -webkit-box-shadow: none;
-    -moz-box-shadow: none;
+  :hover {
+    background-color: var(--primary);
+    color: var(--secondary);
+  }
+`;
+
+export const StyledButtonFoot = styled.button`
+  padding: 10px;
+  border: none;
+  background-color: var(--secondary);
+  padding: 10px;
+  font-weight: bold;
+  color: var(--primary);
+  width: 100px;
+  cursor: pointer;
+  :hover {
+    background-color: var(--primary);
+    color: var(--secondary);
+  }
+  @media (max-width: 766px) {
+    margin-bottom: 24px;
+    max-width: 120px;
+    width: 120px;
   }
 `;
 
@@ -33,7 +48,7 @@ export const StyledRoundButton = styled.button`
   padding: 10px;
   border-radius: 100%;
   border: none;
-  background-color: var(--primary);
+  background-color: var(--secondary);
   padding: 10px;
   font-weight: bold;
   font-size: 15px;
@@ -57,12 +72,13 @@ export const StyledRoundButton = styled.button`
 export const ResponsiveWrapper = styled.div`
   display: flex;
   flex: 1;
-  flex-direction: column;
+  flex-direction: row;
   justify-content: stretched;
   align-items: stretched;
   width: 100%;
-  @media (min-width: 767px) {
-    flex-direction: row;
+  @media (max-width: 766px) {
+    flex-direction: column;
+    flex-direction: column-reverse;
   }
 `;
 
@@ -71,22 +87,37 @@ export const StyledLogo = styled.img`
   @media (min-width: 767px) {
     width: 150px;
   }
+  max-height: 100px;
   transition: width 0.5s;
   transition: height 0.5s;
 `;
 
 export const StyledImg = styled.img`
   box-shadow: 0px 5px 11px 2px rgba(0, 0, 0, 0.7);
-  border: 4px dashed var(--secondary);
   background-color: var(--accent);
-  width: 320px;
+  height: auto;
+  width: 100vw;
   @media (min-width: 900px) {
-    width: 35vw;
+    width: 45vw;
   }
   @media (min-width: 1000px) {
-    width: 35vw;
+    width: 45vw;
   }
   transition: width 0.5s;
+`;
+
+export const StyledCont = styled.div`
+  width: 90%;
+  border: 2px solid var(--primary-text);
+  padding: 24px;
+  display: flex;
+  justify-content: space-around;
+  flex-direction: row;
+  align-items: center;
+  @media (max-width: 766px) {
+    flex-direction: column;
+    align-items: center;
+  }
 `;
 
 export const StyledLink = styled.a`
@@ -121,16 +152,20 @@ function App() {
   });
 
   const claimNFTs = async () => {
-    // console.log(blockchain)
-    // let totalWhiteListed = await store
-    //     .getState()
-    //     .blockchain.smartContract.methods.whiteListMintCount(blockchain.account)
-    //     .call();
-    // if (totalWhiteListed <= 0) {
-    //   setFeedback("Sorry, You are not in the whiteList wait until the public sales beggin.");
-    // } else if (totalWhiteListed < mintAmount) {
-    //   setFeedback(`Sorry, You exceeded your mint account(${totalWhiteListed}).`);
-    // } else {
+    let totalWhiteListed;
+    if (CONFIG.WHITE_LIST_MINT) {
+      totalWhiteListed = await store
+        .getState()
+        .blockchain.smartContract.methods.whiteListMintCount(blockchain.account)
+        .call();
+    } else {
+      totalWhiteListed = mintAmount;
+    }
+    if (totalWhiteListed <= 0) {
+      setFeedback("Sorry, You are not in the whiteList wait untill the public sales beggin.");
+    } else if (totalWhiteListed < mintAmount) {
+      setFeedback(`Sorry, You ecceeded your mint amount${mintAmount}.`);
+    } else {
       let cost = CONFIG.WEI_COST;
       let gasLimit = CONFIG.GAS_LIMIT;
       let totalCostWei = String(cost * mintAmount);
@@ -141,8 +176,7 @@ function App() {
       setClaimingNft(true);
       blockchain.smartContract.methods
         // .mint(blockchain.account, mintAmount)
-        // .whitelistMint(mintAmount)
-        .mintOrc(mintAmount)
+        .totalSupply()
         .send({
           gasLimit: String(totalGasLimit),
           to: CONFIG.CONTRACT_ADDRESS,
@@ -161,34 +195,8 @@ function App() {
           );
           setClaimingNft(false);
         });
-      // }
+      }
   };
-
-  const isWhiteListed = async () => {
-    // blockchain.smartContract.methods
-    // .whiteListMintCount(blockchain.account)
-    // .send({
-    //   from: blockchain.account
-    // })
-    //   .once("error", (err) => {
-    //     console.log(err);
-    //     setFeedback("Sorry, You are not in the whiteList wait untill the public sales beggin.");
-    //   })
-    //   .then((receipt) => {
-    //     console.log(receipt);
-    //     claimNFTs()
-    //   });
-
-    let totalWhiteListed = await store
-        .getState()
-        .blockchain.smartContract.methods.whiteListMintCount(blockchain.account)
-        .call();
-    if (totalWhiteListed >= 0) {
-      this.claimNFTs();
-    } else {
-      setFeedback("Sorry, You are not in the whiteList wait untill the public sales beggin.");
-    }
-  }
 
   const decrementMintAmount = () => {
     let newMintAmount = mintAmount - 1;
@@ -236,14 +244,14 @@ function App() {
       <s.Container
         flex={1}
         ai={"center"}
-        style={{ padding: 24, backgroundColor: "var(--primary)" }}
+        style={{ padding: "24px 0", backgroundColor: "var(--primary)" }}
         image={CONFIG.SHOW_BACKGROUND ? "/config/images/bg.png" : null}
       >
         <StyledLogo alt={"logo"} src={"/config/images/logo.png"} />
         <s.SpacerSmall />
-        <ResponsiveWrapper flex={1} style={{ padding: 24 }} test>
+        <ResponsiveWrapper flex={1} style={{ padding: "24px 0" }}>
           <s.Container flex={1} jc={"center"} ai={"center"}>
-            <StyledImg alt={"example"} src={"/config/images/example.gif"} />
+            <StyledImg alt={"example"} src={"/config/images/examples.png"} />
           </s.Container>
           <s.SpacerLarge />
           <s.Container
@@ -255,6 +263,35 @@ function App() {
               borderRadius: 24,
             }}
           >
+            <s.TextTitle
+              style={{
+                textAlign: "center",
+                fontSize: 30,
+                fontWeight: "bold",
+                color: "var(--accent-text)",
+              }}
+            >
+              {CONFIG.TITLE_ONE}
+            </s.TextTitle>
+            <s.TextTitle
+              style={{
+                textAlign: "center",
+                fontSize: 30,
+                fontWeight: "bold",
+                color: "var(--accent-text)",
+              }}
+            >
+              {CONFIG.TITLE_TWO}
+            </s.TextTitle>
+            <s.TextTitle
+              style={{
+                textAlign: "center",
+                fontSize: "25",
+                color: "var(--accent)",
+              }}
+            >
+              {CONFIG.MAX_PER_WALLET}
+            </s.TextTitle>
             <s.TextTitle
               style={{
                 textAlign: "center",
@@ -342,6 +379,17 @@ function App() {
                         </s.TextDescription>
                       </>
                     ) : null}
+                    <s.TextTitle
+                      style={{
+                        textAlign: "center",
+                        fontSize: 20,
+                        fontWeight: "bold",
+                        color: "var(--accent-text)",
+                        paddingTop: 25
+                      }}
+                    >
+                      {CONFIG.SOLD_OUT}
+                    </s.TextTitle>
                   </s.Container>
                 ) : (
                   <>
@@ -405,6 +453,52 @@ function App() {
             <s.SpacerMedium />
           </s.Container>
         </ResponsiveWrapper>
+        <s.SpacerMedium />
+        <StyledCont>
+          {CONFIG.TWITTER != "" ? (
+            <StyledButtonFoot
+              onClick={(e) => {
+                e.preventDefault();
+                window.open(CONFIG.TWITTER, "_blank");
+              }}
+            >
+              TWITTER
+            </StyledButtonFoot>
+          ):null}
+          {CONFIG.DISCORD != "" ? (
+            <StyledButtonFoot
+              onClick={(e) => {
+                e.preventDefault();
+                window.open(CONFIG.DISCORD, "_blank");
+              }}
+            >
+              DISCORD
+            </StyledButtonFoot>
+          ):null}
+          {CONFIG.OPENSEA != "" ? (
+            <StyledButtonFoot
+              onClick={(e) => {
+                e.preventDefault();
+                window.open(CONFIG.OPENSEA, "_blank");
+              }}
+            >
+              OPENSEA
+            </StyledButtonFoot>
+          ):null}
+          {CONFIG.SCAN_LINK != "" ? (
+            <StyledButtonFoot
+              onClick={(e) => {
+                e.preventDefault();
+                window.open(CONFIG.SCAN_LINK, "_blank");
+              }} 
+              style={{
+                width: 170
+              }}
+            >
+              VERIFY CONTRACT
+            </StyledButtonFoot>
+          ):null}
+        </StyledCont>
         <s.SpacerMedium />
         <s.Container jc={"center"} ai={"center"} style={{ width: "70%" }}>
           <s.TextDescription
